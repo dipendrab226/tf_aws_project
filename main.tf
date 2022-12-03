51c43aed48d83a814b88ea98f0afe47a207bf1f4
dibsbharati <54254648+dibsbharati@users.noreply.github.com>
2022-12-03 13:32:28 -0600

    shared cred file should be list

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   region  = "us-west-2"
-  shared_credentials_files  = "$HOME/.aws/credentials"
+  shared_credentials_files  = ["$HOME/.aws/credentials"]
   profile                   = "default"
 }
 
