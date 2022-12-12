6033c7fc9e01af75fe210fa65a5ef2eea838eaa0
dibsbharati <54254648+dibsbharati@users.noreply.github.com>
2022-12-12 13:26:03 -0600

    add output var for s3 bucket

--- output.tf
+++ output.tf
@@ -3,3 +3,8 @@ output "instance_id" {
     # value = aws_instance.tf_gen_server3
     value = [for s in aws_instance.tf_gen_server3: s.tags]
 }
+
+output "s3_bucket_name"{
+    description = " S3 bucket name"
+    value = [for s in aws_s3_bucket.my_s3_bucket: s.bucket]
+}
