938e35073c4601e881c8f4a4ee226ba87112212e
dibsbharati <54254648+dibsbharati@users.noreply.github.com>
2022-12-12 12:52:29 -0600

    fix output var

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "instance_id" {
     description = "Instance id of the ec2 instance"
-    value = aws_instance.tf_gen_server3.tag_name
+    value = aws_instance.tf_gen_server3[count.index].tag_name
 }
